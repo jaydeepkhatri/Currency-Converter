3f566f3e0c2e7d8d47a819053a60ad7dbacdd877
jaydeepkhatri <jaydeepkhatri79@gmail.com>
2020-08-13 23:57:24 +0530

    ✏ Design edits

--- app.js
+++ app.js
@@ -6,90 +6,101 @@ let maincurrency = maincurrencyEl.value
 const datenowEl = document.querySelector("#datenow");
 const othercurrencys = document.querySelector("#othercurrencys");
 let rates = "";
-// console.log(currencyvalue)
 const currencynames = {
-    ARS: "Argentine peso",
-    AUD: "Australain dollar",
-    BSD: "Bahamian dollar",
-    BRL: "Brazilian real ",
-    GBP: "British pound ",
-    BGN: "Bulgarian lev",
-    CAD: "Canadian dollar",
-    CLP: "Chilean peso",
-    CNY: "Chinese yuan",
-    COP: "Colombian peso",
-    CZK: "Czech koruna",
-    DKK: "Danish krone",
-    DOP: "Dominican peso",
-    EGP: "Egyptian pound",
-    EUR: "euro",
-    FJD: "Falkland Islands pound",
-    GTQ: "Guatemalan quetzal",
-    HKD: "Hong Kong dollar",
-    HRK: "Croatian kuna",
-    HUF: "Hungarian forint",
-    ISK: "Icelandic króna",
-    IDR: "Indonesian rupiah",
-    INR: "Indian rupee",
-    ILS: "Israeli new shekel",
-    JPY: "Japanese yen",
-    KZT: "Kazakhstani tenge",
-    MYR: "Malayasian ringgi",
-    MVR: "Maldivian rufiyaa",
-    MXN: "Mexican peso",
-    NOK: "Norwegian krone",
-    TWD: "New Taiwan dollar",
-    NZD: "New Zealand dollar",
-    PKR: "Pakistani rupee",
-    PAB: "Panamanian balboa",
-    PYG: "Paraguayan guaraní",
-    PEN: "Peruvian sol",
-    PHP: "Philippine peso",
-    PLN: "Polish złoty",
-    RON: "Romanian leu",
-    RUB: "Russian ruble",
-    SAR: "Saudi riyal",
-    SGD: "Singapore dollar",
-    ZAR: "South African rand",
-    KRW: "South Korea won",
-    SEK: "Swedish krona",
-    CHF: "Swiss franc",
-    THB: "Thai baht",
-    TRY: "Turkish lira",
-    AED: "UAE dirham",
-    UAH: "Ukrainian hryvnia",
-    USD: "US dollar",
-    UYU: "Uruguayan peso"
+	ARS: "Argentine peso",
+	AUD: "Australain dollar",
+	BSD: "Bahamian dollar",
+	BRL: "Brazilian real ",
+	GBP: "British pound ",
+	BGN: "Bulgarian lev",
+	CAD: "Canadian dollar",
+	CLP: "Chilean peso",
+	CNY: "Chinese yuan",
+	COP: "Colombian peso",
+	CZK: "Czech koruna",
+	DKK: "Danish krone",
+	DOP: "Dominican peso",
+	EGP: "Egyptian pound",
+	EUR: "euro",
+	FJD: "Falkland Islands pound",
+	GTQ: "Guatemalan quetzal",
+	HKD: "Hong Kong dollar",
+	HRK: "Croatian kuna",
+	HUF: "Hungarian forint",
+	ISK: "Icelandic króna",
+	IDR: "Indonesian rupiah",
+	INR: "Indian rupee",
+	ILS: "Israeli new shekel",
+	JPY: "Japanese yen",
+	KZT: "Kazakhstani tenge",
+	MYR: "Malayasian ringgi",
+	MVR: "Maldivian rufiyaa",
+	MXN: "Mexican peso",
+	NOK: "Norwegian krone",
+	TWD: "New Taiwan dollar",
+	NZD: "New Zealand dollar",
+	PKR: "Pakistani rupee",
+	PAB: "Panamanian balboa",
+	PYG: "Paraguayan guaraní",
+	PEN: "Peruvian sol",
+	PHP: "Philippine peso",
+	PLN: "Polish złoty",
+	RON: "Romanian leu",
+	RUB: "Russian ruble",
+	SAR: "Saudi riyal",
+	SGD: "Singapore dollar",
+	ZAR: "South African rand",
+	KRW: "South Korea won",
+	SEK: "Swedish krona",
+	CHF: "Swiss franc",
+	THB: "Thai baht",
+	TRY: "Turkish lira",
+	AED: "UAE dirham",
+	UAH: "Ukrainian hryvnia",
+	USD: "US dollar",
+	UYU: "Uruguayan peso"
 }
-async function cal() {
-    await fetch(`${url}${maincurrency}`)
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            rates = data.rates;
-            datenowEl.innerHTML = `(${data.date})`;
-            consolefname(rates);
-        })
+async function getexchange() {
+	await fetch(`${url}${maincurrency}`)
+		.then(response => {
+			return response.json();
+		})
+		.then(data => {
+			rates = data.rates;
+			datenowEl.innerHTML = `(${data.date})`;
+			consolefname(rates);
+		})
 }
 maincurrencyEl.addEventListener("change", () => {
-    maincurrency = maincurrencyEl.value;
-    cal();
+	maincurrency = maincurrencyEl.value;
+	getexchange();
 })
 currencyvalueEl.addEventListener("input", () => {
-    currencyvalue = currencyvalueEl.value;
-    cal();
+	currencyvalue = currencyvalueEl.value;
+	getexchange();
 });
 function consolefname(rates) {
-    othercurrencys.innerHTML = "";
+	othercurrencys.innerHTML = "";
+	console.log(rates)
+	Object.keys(rates).forEach((key) => {
+		// console.log(`${currencynames[key]} : ${rates[key]}`);
+		let eachcurrency = document.createElement("div");
+		eachcurrency.classList = "scurrency";
 
-    console.log(rates)
-    Object.keys(rates).forEach((key) => {
-        // console.log(`${currencynames[key]} : ${rates[key]}`);
-        let eachcurrency = document.createElement("div");
-        eachcurrency.classList = "scurrency";
-        eachcurrency.innerHTML = `${currencynames[key]} : ${(rates[key] * currencyvalue).toFixed(2)}`;
-        othercurrencys.append(eachcurrency);
-    });
+		let eachcurrencycountry = document.createElement("span");
+		eachcurrencycountry.innerHTML = currencynames[key];
+		eachcurrency.append(eachcurrencycountry);
+
+		let eachcurrencysymbol = document.createElement("span");
+		eachcurrencysymbol.innerHTML = key;
+		eachcurrency.append(eachcurrencysymbol);
+
+		let eachcurrencyrate = document.createElement("span");
+		eachcurrencyrate.innerHTML = (rates[key] * currencyvalue).toFixed(2);
+		eachcurrency.append(eachcurrencyrate);
+		// eachcurrency.innerHTML = currencynames[key];
+		// eachcurrency.innerHTML = `${currencynames[key]} : ${(rates[key] * currencyvalue).toFixed(2)}`;
+		othercurrencys.append(eachcurrency);
+	});
 }
-cal();
+getexchange();
